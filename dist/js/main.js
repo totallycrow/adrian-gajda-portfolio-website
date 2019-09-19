631d1d4b0875faaae161617963d28af493886541
Adrian Gajda <adrian.gajda@hotmail.com>
2019-09-19 17:10:06 +0100

    Add showcase section and js typewriter effect

---
+++ dist/js/main.js
@@ -0,0 +1,64 @@
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  //   Get full text of current word
+  const fullTxt = this.words[current];
+
+  // Check if deleting
+  if (this.isDeleting) {
+    // Remove a char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add a char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  //   Insert txt into element
+  this.txtElement.innerHTML = `<span class="text-primary txt">${this.txt}</span>`;
+
+  //   Initial Type Speed
+  let typeSpeed = 300;
+  if (this.isDeleting) {
+    typeSpeed /= 3;
+  }
+
+  //   If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // Make a pause at end
+    typeSpeed = this.wait;
+    // Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before start typing
+    typeSpeed = 200;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init On Dom Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+
+  //   Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
